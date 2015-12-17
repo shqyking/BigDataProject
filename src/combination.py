6a12c9fb4379412e0b3d4576a56cba3c017375df
ys2816@columbia.edu <yunsun.seu@gmail.com>
2015-12-17 17:11:43 -0500

    update combination

--- src/combination.py
+++ src/combination.py
@@ -11,10 +11,12 @@ path = '../data/prediction/'
 filename_xgb = 'xgboost'
 filename_nn2 = 'pred_nn2_dataset'
 filename_com = 'com'
+filename_rf = 'randomForest'
 
 for idx in range(0, 10):
 	y_xgb = load_pred_data(path + filename_xgb + str(idx) + '.csv')
 	y_nn2 = load_pred_data(path + filename_nn2 + str(idx) + '.csv')
-	y_prob = np.multiply(y_xgb,0.5) + np.multiply(y_nn2,0.5) 
+	y_rf = load_pred_data(path + filename_rf + str(idx) + '.csv')
+	y_prob = np.multiply(y_xgb,0.34) + np.multiply(y_nn2,0.33) + np.multiply(y_rf, 0.33)
 	my_df = pd.DataFrame(y_prob)
 	my_df.to_csv(path + filename_com + str(idx) + '.csv', index = False, header = True)
